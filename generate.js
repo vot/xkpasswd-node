b0953cac2b3b5593ec28230d348065218fff87a5
Vot Z <vot@siliconfen.co>
2015-10-14 22:52:56 +0100

    Simplified the code a tiny bit

--- generate.js
+++ generate.js
@@ -1,14 +1,7 @@
 var wordList = require('word-list-json');
 
-/**
- * Patterns can consist of any combination of the following:
- * w: words
- * d: digits
- * s: separators
- */
-
-
-var helpers = {
+// define helpers
+var h = {
   random: function (min, max) {
     // Returns a random integer between min (included) and max (included)
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -16,56 +9,62 @@ var helpers = {
   },
 
   getRandomWord: function () {
-    return wordList[helpers.random(0, wordList.length - 1)];
+    return wordList[h.random(0, wordList.length - 1)];
+  },
+
+  resolveComplexity: function (complexity) {
+    // Patterns can consist of any combination of the following: (w)ords, (d)igits, (s)eparators
+    var rtn = {};
+    rtn.separators = '#.-=+';
+    if (complexity < 1) complexity = 1;
+    if (complexity > 6) complexity = 6;
+
+    if (complexity === 1) rtn.pattern = 'wsw';
+    if (complexity === 2) rtn.pattern = 'wswsdd';
+    if (complexity === 3) rtn.pattern = 'wswswsdd';
+    if (complexity === 4) rtn.pattern = 'swswswswswsdd';
+
+    if (complexity === 5) {
+      rtn.pattern = 'swswswswswsdd'
+      rtn.separators = '#!@$*+:|~?';
+    };
+
+    if (complexity === 6) {
+      rtn.pattern = 'ddswswswswswsdd';
+      rtn.transform = 'alternate';
+      rtn.separators = '!@$%^&*-_+=:|~?.;';
+    }
+
+    return rtn;
   },
 
   processOpts: function (opts) {
-    // define defaults
     if (!opts.complexity) opts.complexity = 3;
-    var defaultSeparators = '#.-=+';
+    var predefined = h.resolveComplexity(opts.complexity);
+    var separators = (opts.separators || predefined.separators);
     var rtn = {};
 
-    // check for complexity level
-    // TODO: Add some sensible patterns
-    if (opts.complexity === 1) rtn.pattern = 'wsw';
-    if (opts.complexity === 2) rtn.pattern = 'wswsdd';
-    if (opts.complexity === 3) rtn.pattern = 'wswswsdd';
-    if (opts.complexity === 4) rtn.pattern = 'swswswswswsdd';
+    rtn.pattern = (opts.pattern || predefined.pattern);
+    rtn.separator = separators.split('')[h.random(0, separators.length - 1)];
+    if (predefined.transform) rtn.transform = predefined.transform;
 
-    if (opts.complexity === 5) {
-      rtn.pattern = 'swswswswswsdd'
-      defaultSeparators = '#!@$*+:|~?';
-    };
-
-    if (opts.complexity === 6) {
-      rtn.pattern = 'ddswswswswswsdd';
-      rtn.transform = 'alternate';
-      defaultSeparators = '!@$%^&*-_+=:|~?.;';
-    }
-
-    // Check if custom pattern is provided
-    if (opts.pattern) rtn.pattern = opts.pattern;
-
-    // Pick a random separator from supplied list
-    separators = (opts.separators || defaultSeparators);
-    rtn.separator = separators.split('')[helpers.random(0, separators.length - 1)];
     return rtn;
   }
 }
 
 
 module.exports = function (opts) {
-  o = helpers.processOpts(opts);
+  o = h.processOpts(opts);
   var pattern = o.pattern.split('');
   var uppercase = (o.transform && o.transform == 'uppercase');
   var password = [];
 
   pattern.forEach(function (type) {
     var value;
-    if (type === 'd') value = helpers.random(0, 9);
+    if (type === 'd') value = h.random(0, 9);
     if (type === 's') value = o.separator;
     if (type === 'w') {
-      value = helpers.getRandomWord();
+      value = h.getRandomWord();
       if (o.transform && o.transform == 'alternate') uppercase = !uppercase;
       if (uppercase) {
         value.toUpperCase();
